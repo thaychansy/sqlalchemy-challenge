a1e8a79a45bd3993c5233225c8d43a0b3f8523aa
thaychansy <thay.chansy@gmail.com>
2024-07-15 08:28:08 -0700

    Update

--- surfsup/app.py
+++ surfsup/app.py
@@ -1,28 +1,43 @@
 # Import the dependencies.
-
-
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+# Create an engine that connects to the SQLite database
+#################################################
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
+
+# View all of the classes that automap found
+Base.classes.keys()
 
 # Save references to each table
+Measurement = Base.classes.measurement
 
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
